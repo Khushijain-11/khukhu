579030cd41cd35b4e916182698fcae8d44fbb451
Khushijain-11 <khushimootha11082004@gmail.com>
2025-03-18 12:44:05 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,5 +1,11 @@
-public class file{
-	public static void main(String[] args){
-		system.out.println("helloworld");
-	}
+ public class file{
+        public static void main (String[] args){
+                int h=3;
+                int l=2;
+                int area=0;
+                area=0.5*l*h;
+                System.out.println(area);
+        }
 }
+~
+~
